332cbbd4e891aca92409b97c4141259345c01449
hari chandra prasad chowdary chandra <chandrahari321@gmail.com>
2024-02-27 19:03:30 +0530

    automated commit #605

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ resource "aws_spot_instance_request" "example" {
   key_name        = "my_keypair"              # Example key pair name
   security_groups = ["sg-0b792d7d432d8d378"]  # Example security group ID
 
-  spot_options {
-    instance_type           = "one-time"       # Example spot instance type (one-time)
-    instance_interruption_behavior = "terminate" # Example instance interruption behavior
-  }
+#  spot_options {
+#    instance_type           = "one-time"       # Example spot instance type (one-time)
+#    instance_interruption_behavior = "terminate" # Example instance interruption behavior
+#  }
 
   tags = {
     Name = "example-spot-instance"            # Example tag
