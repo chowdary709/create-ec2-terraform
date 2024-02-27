a03eb172c9860ba463cdea877292b008c6884334
hari chandra prasad chowdary chandra <chandrahari321@gmail.com>
2024-02-27 20:02:51 +0530

    automated commit #613

--- main.tf
+++ main.tf
@@ -1,14 +1,25 @@
-resource "aws_spot_instance_request" "cheap_worker" {
-  ami             = "ami-0f3c7d07486cad139"
-  instance_type   = "t3.small"
-  subnet_id       = "subnet-017ad3134d13bee7a"
-  key_name        = "artifactory"
-  security_groups = ["sg-0b792d7d432d8d378"]
+resource "aws_instance" "instance" {
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.medium"
+  subnet_id              = "subnet-0233174d26bc6112d"
+  associate_public_ip_address = true
+#  key_name               = "your_key_pair_name"
 
-  spot_options {
-    spot_type                      = "persistent"  # Can be "one-time" or "persistent"
-    instance_interruption_behavior = "stop"  # Can be "terminate" or "stop"# Optional tags
+  vpc_security_group_ids = [
+    "sg-0b792d7d432d8d378"
+  ]
+
   tags = {
     Name = "artifactory"
+  }
+
+  // Spot instance options
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      max_price             = "0" // No maximum price
+      instance_interruption_behavior = "stop"
+      spot_instance_type    = "persistent"
+    }
   }
 }
