db2d94b2900b8a029a8a34f6340000d65896b54d
hari chandra prasad chowdary chandra <chandrahari321@gmail.com>
2024-02-27 19:16:38 +0530

    automated commit #606

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
-resource "aws_spot_instance_request" "example" {
-  ami             = "ami-0f3c7d07486cad139"   # Example AMI ID
-  instance_type   = "t3.micro"                # Example instance type
-  subnet_id       = "subnet-017ad3134d13bee7a" # Example subnet ID
-  key_name        = "my_keypair"              # Example key pair name
-  security_groups = ["sg-0b792d7d432d8d378"]  # Example security group ID
+resource "aws_spot_instance_request" "cheap_worker" {
+  ami             = "ami-0f3c7d07486cad139"
+  instance_type   = "t3.small"
+  subnet_id       = "subnet-017ad3134d13bee7a"
+  key_name        = "artifactory"
+  security_groups = ["sg-0b792d7d432d8d378"]
 
-#  spot_options {
-#    instance_type           = "one-time"       # Example spot instance type (one-time)
-#    instance_interruption_behavior = "terminate" # Example instance interruption behavior
-#  }
-
+  spot_options {
+    spot_type                      = "persistent"  # Can be "one-time" or "persistent"
+    instance_interruption_behavior = "stop"  # Can be "terminate" or "stop"# Optional tags
   tags = {
-    Name = "example-spot-instance"            # Example tag
+    Name = "artifactory"
   }
 }
-
