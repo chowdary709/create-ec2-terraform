4deb15825c1c2895e5cb52c0ef08379830469a8b
chowdary709 <chandrahari321@gmail.com>
2024-03-03 14:48:31 +0530

    automated commit #30

--- main.tf
+++ main.tf
@@ -1,7 +1,28 @@
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#  owners      = ["666171310914"]
+#}
+#
+#module "ec2" {
+#  source                         = "./modules/ec2_instance"
+#  ami                            = data.aws_ami.ami.id
+#  subnet_id                      = var.subnet_id
+#  vpc_security_group_ids         = var.vpc_security_group_ids
+#  instance_interruption_behavior = var.instance_interruption_behavior
+#  spot_instance_type             = var.spot_instance_type
+#  instance_type                  = "t3.medium"
+#  instance_tag_name              = "artifactory"
+#}
+#
+
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["666171310914"]
+  filter {
+    name   = "name"
+    values = ["*Centos-8-DevOps-Practice*"]
+  }
 }
 
 module "ec2" {
