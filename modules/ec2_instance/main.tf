3be7ab586afc6b24fe061da3eb00a03c92ad44e5
chowdary709 <chandrahari321@gmail.com>
2024-03-03 14:12:45 +0530

    automated commit #20

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,9 +1,20 @@
+data "aws_ami" "centos" {
+  most_recent      = true
+  filter {
+    name   = "name"
+    values = ["*Centos-8-DevOps-Practice*"]
+  }
+  filter {
+    name   = "executable-users"
+    values = ["666171310914"]
+  }
+}
+
 resource "aws_instance" "instance" {
-  ami                    = data.aws_ami.centos
+  ami                    = data.aws_ami.centos.id // Use .id to get the AMI ID
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   associate_public_ip_address = true
-
 
   vpc_security_group_ids = var.vpc_security_group_ids
 
@@ -15,9 +26,9 @@ resource "aws_instance" "instance" {
   instance_market_options {
     market_type = "spot"
     spot_options {
-      max_price             = "0" // No maximum price
-      instance_interruption_behavior = var.instance_interruption_behavior
-      spot_instance_type    = var.spot_instance_type
+      max_price                        = "0" // No maximum price
+      instance_interruption_behavior   = var.instance_interruption_behavior
+      spot_instance_type               = var.spot_instance_type
     }
   }
 }
