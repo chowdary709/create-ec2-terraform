5faf05075cac36aa2cbe504a32ef3b546294fc73
chowdary709 <chandrahari321@gmail.com>
2024-03-05 13:00:17 +0530

    automated commit #77

--- data.tf
+++ data.tf
@@ -3,3 +3,34 @@ data "aws_ami" "centos" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 }
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-709"
+#    key    = "misc/jenkins-ip/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
+#
+##data "aws_instance" "instance" {
+##  instance_id = "i-065c4ac84713c7ecd"
+##}
+##
+##resource "aws_route53_record" "jenkins" {
+##  name = "jenkins.roboshop.internal"
+##  type = "A"
+##  zone_id = "Z08360431XA1BOY4SK2N0"
+##  records = [data.aws_instance.instance.public_ip]
+##  ttl = 10
+##}
+#
+#data "aws_instance" "artifactory" {
+#  instance_id = "i-0525a32312f8adeba"
+#}
+#
+#resource "aws_route53_record" "artifactory" {
+#  name = "artifactory.roboshop.internal"
+#  type = "A"
+#  zone_id = "Z08360431XA1BOY4SK2N0"
+#  records = [data.aws_instance.artifactory.public_ip]
+#  ttl = 10
+#}
