37cdb951e0323a0db0994e1e3763c76f4599f8e7
chowdary709 <chandrahari321@gmail.com>
2024-03-15 09:49:36 +0530

    update

--- main.tf
+++ main.tf
@@ -18,26 +18,26 @@ module "frontend" {
 
 
 
-module "backend" {
-  source                         = "./modules/ec2_instance"
-  ami                            = var.ami
-  subnet_id                      = var.subnet_id
-  vpc_security_group_ids         = var.vpc_security_group_ids
-  instance_interruption_behavior = var.instance_interruption_behavior
-  spot_instance_type             = var.spot_instance_type
-  instance_type                  = "t2.micro"
-  instance_tag_name              = "backend"
-  route53_record_name            = "backend.roboshop.internal"
-}
-module "mysql" {
-  source                         = "./modules/ec2_instance"
-  ami                            = var.ami
-  subnet_id                      = var.subnet_id
-  vpc_security_group_ids         = var.vpc_security_group_ids
-  instance_interruption_behavior = var.instance_interruption_behavior
-  spot_instance_type             = var.spot_instance_type
-  instance_type                  = "t2.micro"
-  instance_tag_name              = "mysql"
-  route53_record_name            = "mysql.roboshop.internal"
-}
+# module "backend" {
+#   source                         = "./modules/ec2_instance"
+#   ami                            = var.ami
+#   subnet_id                      = var.subnet_id
+#   vpc_security_group_ids         = var.vpc_security_group_ids
+#   instance_interruption_behavior = var.instance_interruption_behavior
+#   spot_instance_type             = var.spot_instance_type
+#   instance_type                  = "t2.micro"
+#   instance_tag_name              = "backend"
+#   route53_record_name            = "backend.roboshop.internal"
+# }
+# module "mysql" {
+#   source                         = "./modules/ec2_instance"
+#   ami                            = var.ami
+#   subnet_id                      = var.subnet_id
+#   vpc_security_group_ids         = var.vpc_security_group_ids
+#   instance_interruption_behavior = var.instance_interruption_behavior
+#   spot_instance_type             = var.spot_instance_type
+#   instance_type                  = "t2.micro"
+#   instance_tag_name              = "mysql"
+#   route53_record_name            = "mysql.roboshop.internal"
+# }
 
