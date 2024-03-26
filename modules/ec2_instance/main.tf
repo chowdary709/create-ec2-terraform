509cd68dd722c1135e35ea7183a64851f295ca35
chowdary709 <chandrahari321@gmail.com>
2024-03-26 20:20:18 +0530

    automated commit #163

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "instance" {
     #!/bin/bash
     dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
     pip3.11 install boto3 botocore | tee -a /opt/userdata.log
-    ansible-pull -i localhost, -U https://github.com/chowdary709/expense-ansible  expense.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
+    ansible-pull -i localhost, -U https://github.com/chowdary709/expense-ansible  expense.yml -e role_name=${role_name} -e env=${ component } | tee -a /opt/userdata.log
     EOF
 
   // Spot instance options
