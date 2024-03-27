aa1e47d0eeb602a673fc8772e4d89672d5cdaca3
chowdary709 <chandrahari321@gmail.com>
2024-03-27 10:09:03 +0530

    automated commit #178

--- main.tf
+++ main.tf
@@ -4,17 +4,17 @@
 #  owners      = ["666171310914"]
 #}
 #
-#module "frontend" {
-#  source                         = "./modules/ec2_instance"
-#  ami                            = var.ami
-#  subnet_id                      = var.subnet_id
-#  vpc_security_group_ids         = var.vpc_security_group_ids
-#  instance_interruption_behavior = var.instance_interruption_behavior
-#  spot_instance_type             = var.spot_instance_type
-#  instance_type                  = "t2.micro"
-#  instance_tag_name              = "frontend"
-#  route53_record_name            = "frontend.roboshop.internal"
-#}
+module "frontend" {
+  source                         = "./modules/ec2_instance"
+  ami                            = var.ami
+  subnet_id                      = var.subnet_id
+  vpc_security_group_ids         = var.vpc_security_group_ids
+  instance_interruption_behavior = var.instance_interruption_behavior
+  spot_instance_type             = var.spot_instance_type
+  instance_type                  = "t2.micro"
+  instance_tag_name              = "frontend"
+  route53_record_name            = "frontend.roboshop.internal"
+}
 #
 #
 #
