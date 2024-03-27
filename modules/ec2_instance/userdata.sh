2885160bcca3c8c07b36e78849a18279cf8b8227
chowdary709 <chandrahari321@gmail.com>
2024-03-27 16:33:02 +0530

    automated commit #193

--- modules/ec2_instance/userdata.sh
+++ modules/ec2_instance/userdata.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-sudo dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
-sudo pip3.11 install boto3 botocore | tee -a /opt/userdata.log
-sudo ansible-pull -i localhost, -U https://github.com/chowdary709/expense-ansible expense.yml -e role_name=${var.role_name} -e env=${var.component} | tee -a /opt/userdata.log
-EOF
+##!/bin/bash
+#sudo dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
+#sudo pip3.11 install boto3 botocore | tee -a /opt/userdata.log
+#sudo ansible-pull -i localhost, -U https://github.com/chowdary709/expense-ansible expense.yml -e role_name=${var.role_name} -e env=${var.component} | tee -a /opt/userdata.log
+#EOF
