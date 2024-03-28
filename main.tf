9c21714acc3395c40c34ae1c7e760aff098d8ffc
chowdary709 <chandrahari321@gmail.com>
2024-03-28 11:32:18 +0530

    automated commit #243

--- main.tf
+++ main.tf
@@ -41,15 +41,15 @@
    route53_record_name            = "mysql.roboshop.internal"
  }
 
-module "elk" {
-  source                         = "./modules/ec2_instance"
-  ami                            = var.ami
-  subnet_id                      = var.subnet_id
-  vpc_security_group_ids         = var.vpc_security_group_ids
-  instance_interruption_behavior = var.instance_interruption_behavior
-  spot_instance_type             = var.spot_instance_type
-  instance_type                  = "r5.large"
-  instance_tag_name              = "elk"
-  route53_record_name            = "elk.roboshop.internal"
-}
+  module "elk" {
+    source                         = "./modules/ec2_instance"
+    ami                            = var.ami
+    subnet_id                      = var.subnet_id
+    vpc_security_group_ids         = var.vpc_security_group_ids
+    instance_interruption_behavior = var.instance_interruption_behavior
+    spot_instance_type             = var.spot_instance_type
+    instance_type                  = "r5.large"
+    instance_tag_name              = "elk"
+    route53_record_name            = "elk.roboshop.internal"
+  }
 
