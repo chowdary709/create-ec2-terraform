a53f5edeaaef500151b99974fc8afa5be8140892
chowdary709 <chandrahari321@gmail.com>
2024-04-03 16:19:53 +0530

    automated commit #65

--- main.tf
+++ main.tf
@@ -4,6 +4,18 @@
 #  owners      = ["666171310914"]
 #}
 #
+module "redis" {
+  source                         = "./modules/ec2_instance"
+  ami                            = var.ami
+  subnet_id                      = var.subnet_id
+  vpc_security_group_ids         = var.vpc_security_group_ids
+  instance_interruption_behavior = var.instance_interruption_behavior
+  spot_instance_type             = var.spot_instance_type
+  instance_type                  = "t2.micro"
+  instance_tag_name              = "redis"
+  route53_record_name            = "redis.roboshop.internal"
+}
+
 module "frontend" {
   source                         = "./modules/ec2_instance"
   ami                            = var.ami
