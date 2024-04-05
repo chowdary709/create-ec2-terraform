5ca758a0a21e2bec497c379ea6f115bc748399ec
chowdary709 <chandrahari321@gmail.com>
2024-04-05 13:29:38 +0530

    automated commit #138

--- main.tf
+++ main.tf
@@ -1,37 +1,3 @@
-#data "aws_ami" "ami" {
-#  most_recent = true
-#  name_regex  = "Centos-8-DevOps-Practice"
-#  owners      = ["666171310914"]
-#}
-#
-
-
-module "web" {
-  source                         = "./modules/ec2_instance"
-  ami                            = var.ami
-  subnet_id                      = var.subnet_id
-  vpc_security_group_ids         = var.vpc_security_group_ids
-  instance_interruption_behavior = var.instance_interruption_behavior
-  spot_instance_type             = var.spot_instance_type
-  instance_type                  = "t2.micro"
-  instance_tag_name              = "web"
-  route53_record_name            = "web.roboshop.internal"
-}
-
-
-
-module "redis" {
-  source                         = "./modules/ec2_instance"
-  ami                            = var.ami
-  subnet_id                      = var.subnet_id
-  vpc_security_group_ids         = var.vpc_security_group_ids
-  instance_interruption_behavior = var.instance_interruption_behavior
-  spot_instance_type             = var.spot_instance_type
-  instance_type                  = "t2.micro"
-  instance_tag_name              = "redis"
-  route53_record_name            = "redis.roboshop.internal"
-}
-
 module "frontend" {
   source                         = "./modules/ec2_instance"
   ami                            = var.ami
@@ -43,31 +9,6 @@ module "frontend" {
   instance_tag_name              = "frontend"
   route53_record_name            = "frontend.roboshop.internal"
 }
-
-module "mongodb" {
-  source                         = "./modules/ec2_instance"
-  ami                            = var.ami
-  subnet_id                      = var.subnet_id
-  vpc_security_group_ids         = var.vpc_security_group_ids
-  instance_interruption_behavior = var.instance_interruption_behavior
-  spot_instance_type             = var.spot_instance_type
-  instance_type                  = "t2.micro"
-  instance_tag_name              = "mongodb"
-  route53_record_name            = "mongodb.roboshop.internal"
-}
-
-module "catalogue" {
-  source                         = "./modules/ec2_instance"
-  ami                            = var.ami
-  subnet_id                      = var.subnet_id
-  vpc_security_group_ids         = var.vpc_security_group_ids
-  instance_interruption_behavior = var.instance_interruption_behavior
-  spot_instance_type             = var.spot_instance_type
-  instance_type                  = "t2.micro"
-  instance_tag_name              = "catalogue"
-  route53_record_name            = "catalogue.roboshop.internal"
-}
-
 
 module "backend" {
  source                         = "./modules/ec2_instance"
--- modules/ec2_instance/provider.tf
+++ modules/ec2_instance/provider.tf
@@ -1,3 +1,3 @@
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region code
+  region = "us-east-1"
 }
