cbd51e55b2dfee065f977cbaa8b3bf1742fe0a23
chowdary709 <chandrahari321@gmail.com>
2024-04-05 16:20:45 +0530

    automated commit #144

--- roboshop/input.tfvars
+++ roboshop/input.tfvars
@@ -5,29 +5,6 @@ instance_interruption_behavior = "stop"
 spot_instance_type     = "persistent"
 
 
-#resource "aws_instance" "instance" {
-#  ami                    = ami-0f3c7d07486cad139
-#  instance_type          = var.instance_type
-#  subnet_id              = var.subnet_id
-#  associate_public_ip_address = true
-#
-#
-#  vpc_security_group_ids = [ "sg-0b792d7d432d8d378" ]
-#
-#  tags = {
-#    Name = "artifactory"
-#  }
-#
-#  // Spot instance options
-#  instance_market_options {
-#    market_type = "spot"
-#    spot_options {
-#      max_price             = "0" // No maximum price
-#      instance_interruption_behavior = "stop"
-#      spot_instance_type    = "persistent"
-#    }
-#  }
-#}
 
 
 
--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,4 +1,4 @@
-module "frontend" {
+module "web" {
   source                         = "./modules/ec2_instance"
   ami                            = var.ami
   subnet_id                      = var.subnet_id
@@ -6,43 +6,7 @@ module "frontend" {
   instance_interruption_behavior = var.instance_interruption_behavior
   spot_instance_type             = var.spot_instance_type
   instance_type                  = "t2.micro"
-  instance_tag_name              = "frontend"
-  route53_record_name            = "frontend.roboshop.internal"
-}
-
-module "backend" {
- source                         = "./modules/ec2_instance"
- ami                            = var.ami
- subnet_id                      = var.subnet_id
- vpc_security_group_ids         = var.vpc_security_group_ids
- instance_interruption_behavior = var.instance_interruption_behavior
- spot_instance_type             = var.spot_instance_type
- instance_type                  = "t2.micro"
- instance_tag_name              = "backend"
- route53_record_name            = "backend.roboshop.internal"
-}
-
-module "mysql" {
- source                         = "./modules/ec2_instance"
- ami                            = var.ami
- subnet_id                      = var.subnet_id
- vpc_security_group_ids         = var.vpc_security_group_ids
- instance_interruption_behavior = var.instance_interruption_behavior
- spot_instance_type             = var.spot_instance_type
- instance_type                  = "t2.micro"
- instance_tag_name              = "mysql"
- route53_record_name            = "mysql.roboshop.internal"
-}
-
-module "elk" {
-  source                         = "./modules/ec2_instance"
-  ami                            = var.ami
-  subnet_id                      = var.subnet_id
-  vpc_security_group_ids         = var.vpc_security_group_ids
-  instance_interruption_behavior = var.instance_interruption_behavior
-  spot_instance_type             = var.spot_instance_type
-  instance_type                  = "r5.large"
-  instance_tag_name              = "elk"
-  route53_record_name            = "elk.roboshop.internal"
+  instance_tag_name              = "web"
+  route53_record_name            = "web.roboshop.internal"
 }
 
