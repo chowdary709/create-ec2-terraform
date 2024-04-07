4d8c51d855c89494f64fcb656671ad9d308d0234
chowdary709 <chandrahari321@gmail.com>
2024-04-07 21:41:15 +0530

    automated commit #194

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -2,7 +2,7 @@ module "elk" {
   source                         = "./modules/ec2_instance"
   ami                            = var.ami
   subnet_id                      = var.subnet_id
-   vpc_security_group_ids         = var.us-east-1c
+   vpc_security_group_ids         = "var.us-east-1c"
   instance_interruption_behavior = var.instance_interruption_behavior
   spot_instance_type             = var.spot_instance_type
   instance_type                  = "t3.medium"
