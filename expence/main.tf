44d9d98ff4607308dd3f108fc4e9b9ec30baac72
chowdary709 <chandrahari321@gmail.com>
2024-04-07 23:18:31 +0530

    automated commit #201

--- expence/main.tf
+++ expence/main.tf
@@ -34,3 +34,14 @@ module "backend" {
  instance_tag_name              = "backend"
  route53_record_name            = "backend.roboshop.internal"
 }
+module "elk" {
+  source                         = "./modules/ec2_instance"
+  ami                            = var.ami
+  subnet_id                      = var.us-east-1c
+  vpc_security_group_ids         = var.vpc_security_group_ids
+  instance_interruption_behavior = var.instance_interruption_behavior
+  spot_instance_type             = var.spot_instance_type
+  instance_type                  = "t3.medium"
+  instance_tag_name              = "elk"
+  route53_record_name            = "elk.roboshop.internal"
+}
