55c85e04e88433c07fbebd96b9e01f7b604e4b64
chowdary709 <chandrahari321@gmail.com>
2024-04-15 20:35:12 +0530

    automated commit #267

--- roboshop/state.tf
+++ roboshop/state.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "terraform-709"
-    key            = "expense-project/roboshop_ec2/terraform.tfstate"
-    region         = "us-east-1"
-  }
+  backend "s3" {}
 }
