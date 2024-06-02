3085140b0b1500dc7c8c9e1b985c3754591f4ec5
chowdary709 <chandrahari321@gmail.com>
2024-06-02 17:05:24 +0530

    update

--- devlovep/vars.tf
+++ devlovep/vars.tf
@@ -6,4 +6,5 @@ variable "instance_tag_name" {}
 variable"instance_interruption_behavior" {}
 variable"spot_instance_type" {}
 variable "route53_record_name" {}
-
+variable "azs" {}
+variable "component" {}
--- roboshop/main.tf
+++ roboshop/main.tf
@@ -10,27 +10,27 @@ module "web" {
   route53_record_name            = "web.chowdary.cloud"
 }
 
-# module "mongodb" {
-#   source                         = "./modules/ec2_instance"
-#   ami                            = var.ami
-#   subnet_id                      = var.us-east-1c
-#   vpc_security_group_ids         = var.vpc_security_group_ids
-#   instance_interruption_behavior = var.instance_interruption_behavior
-#   spot_instance_type             = var.spot_instance_type
-#   instance_type                  = "t2.micro"
-#   instance_tag_name              = "mongodb"
-#   route53_record_name            = "mongodb.roboshop.internal"
-# }
-#
-# module "catalogue" {
-#   source                         = "./modules/ec2_instance"
-#   ami                            = var.ami
-#   subnet_id                      = var.us-east-1c
-#   vpc_security_group_ids         = var.vpc_security_group_ids
-#   instance_interruption_behavior = var.instance_interruption_behavior
-#   spot_instance_type             = var.spot_instance_type
-#   instance_type                  = "t2.micro"
-#   instance_tag_name              = "catalogue"
-#   route53_record_name            = "catalogue.roboshop.internal"
-# }
-#
+module "mongodb" {
+  source                         = "./modules/ec2_instance"
+  ami                            = var.ami
+  subnet_id                      = var.us-east-1c
+  vpc_security_group_ids         = var.vpc_security_group_ids
+  instance_interruption_behavior = var.instance_interruption_behavior
+  spot_instance_type             = var.spot_instance_type
+  instance_type                  = "t2.micro"
+  instance_tag_name              = "mongodb"
+  route53_record_name            = "mongodb.chowdary.cloud"
+}
+
+module "catalogue" {
+  source                         = "./modules/ec2_instance"
+  ami                            = var.ami
+  subnet_id                      = var.us-east-1c
+  vpc_security_group_ids         = var.vpc_security_group_ids
+  instance_interruption_behavior = var.instance_interruption_behavior
+  spot_instance_type             = var.spot_instance_type
+  instance_type                  = "t2.micro"
+  instance_tag_name              = "catalogue"
+  route53_record_name            = "catalogue.chowdary.cloud"
+}
+
