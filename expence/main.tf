753206e66cd463c2d1a9f060514086a2a3da276f
chowdary709 <chandrahari321@gmail.com>
2024-06-25 15:25:23 +0530

    automated commit #2857

--- expence/main.tf
+++ expence/main.tf
@@ -1,7 +1,7 @@
 module "jenkins" {
   source                         = "./modules/ec2_instance"
   ami                            = var.ami
-  subnet_id                      = var.us-east-1cc
+  subnet_id                      = var.us-east-1c
   vpc_security_group_ids         = var.vpc_security_group_ids
   instance_interruption_behavior = var.instance_interruption_behavior
   spot_instance_type             = var.spot_instance_type
