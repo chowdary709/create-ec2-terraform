2116d64a07874c9eb7ab86f81bd565f2c0f6fbc3
chowdary709 <chandrahari321@gmail.com>
2024-06-25 15:24:45 +0530

    automated commit #2854

--- expence/main.tf
+++ expence/main.tf
@@ -1,52 +1,27 @@
-# module "frontend" {
-#   source                         = "./modules/ec2_instance"
-#   ami                            = var.ami
-#   subnet_id                      = var.us-east-1c
-#   vpc_security_group_ids         = var.vpc_security_group_ids
-#   instance_interruption_behavior = var.instance_interruption_behavior
-#   spot_instance_type             = var.spot_instance_type
-#   instance_type                  = "t2.micro"
-#   instance_tag_name              = "frontend"
-#   route53_record_name            = "frontend.roboshop.internal"
-# }
-
-
-# module "mysql" {
-#  source                         = "./modules/ec2_instance"
-#  ami                            = var.ami
-#  subnet_id                      = var.us-east-1c
-#   vpc_security_group_ids         = var.vpc_security_group_ids
-#  instance_interruption_behavior = var.instance_interruption_behavior
-#  spot_instance_type             = var.spot_instance_type
-#  instance_type                  = "t3.medium"
-#  instance_tag_name              = "mysql"
-#  route53_record_name            = "mysql.roboshop.internal"
-# }
-
-# module "backend" {
-#  source                         = "./modules/ec2_instance"
-#  ami                            = var.ami
-#  subnet_id                      = var.us-east-1c
-#   vpc_security_group_ids         = var.vpc_security_group_ids
-#  instance_interruption_behavior = var.instance_interruption_behavior
-#  spot_instance_type             = var.spot_instance_type
-#  instance_type                  = "t2.micro"
-#  instance_tag_name              = "backend"
-#  route53_record_name            = "backend.roboshop.internal"
-# }
-
-
-module "elk" {
+module "jenkins" {
   source                         = "./modules/ec2_instance"
   ami                            = var.ami
-  subnet_id                      = var.us-east-1a
+  subnet_id                      = var.us-east-1c
   vpc_security_group_ids         = var.vpc_security_group_ids
   instance_interruption_behavior = var.instance_interruption_behavior
   spot_instance_type             = var.spot_instance_type
-  instance_type                  = "t3.large"
-  instance_tag_name              = "elk"
-  route53_record_name            = "elk.chowdary.cloud"
+  instance_type                  = "t3.small"
+  instance_tag_name              = "jenkins"
+  route53_record_name            = "jenkins.chowdary.cloud"
 }
+
+
+# module "elk" {
+#   source                         = "./modules/ec2_instance"
+#   ami                            = var.ami
+#   subnet_id                      = var.us-east-1a
+#   vpc_security_group_ids         = var.vpc_security_group_ids
+#   instance_interruption_behavior = var.instance_interruption_behavior
+#   spot_instance_type             = var.spot_instance_type
+#   instance_type                  = "t3.large"
+#   instance_tag_name              = "elk"
+#   route53_record_name            = "elk.chowdary.cloud"
+# }
 
 # module "prometheus" {
 #   source                         = "./modules/ec2_instance"
@@ -57,5 +32,5 @@ module "elk" {
 #   spot_instance_type             = var.spot_instance_type
 #   instance_type                  = "t3.small"
 #   instance_tag_name              = "prometheus"
-#   route53_record_name            = "prometheus.roboshop.internal"
+#   route53_record_name            = "prometheus.chowdary.cloud"
 # }
--- roboshop/state.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "terraform-709"
-key    = "expense-project/roboshop_ec2/terraform.tfstate"
-region = "us-east-1"
