c8f59d1b9570916f2f0e5f7aefa2c46c5d850ebc
chowdary709 <chandrahari321@gmail.com>
2024-06-30 18:51:30 +0530

    automated commit #3042

--- expense-misc/data.tf
+++ expense-misc/data.tf
@@ -1,20 +1,3 @@
-data "aws_instances" "Workstation" {
-  filter {
-    name   = "tag:Name"
-    values = ["Workstation"]
-  }
-}
-
-data "aws_instances" "Jenkins" {
-  filter {
-    name   = "tag:Name"
-    values = ["Jenkins"]
-  }
-}
-
-data "aws_instances" "Sonarqube" {
-  filter {
-    name   = "tag:Name"
-    values = ["Sonarqube"]
-  }
+data "aws_instance" "Workstation" {
+  instance_id = "i-0769bb47be7608385"
 }
--- expense-misc/main.tf
+++ expense-misc/main.tf
@@ -1,8 +1,12 @@
-
 resource "aws_route53_record" "workstation" {
   name    = "workstation.chowdary.cloud"
   type    = "A"
   zone_id = "Z0013695SMHQDK42GJB1"
-  records = [data.aws_instances.Workstation.instances[0].public_ip]
+  records = [data.aws_instance.Workstation.public_ip]
   ttl     = 10
 }
+
+
+
+
+
