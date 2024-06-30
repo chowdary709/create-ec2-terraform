5c8b0dd11af5fea1ab538747ee61cb3550c9901a
chowdary709 <chandrahari321@gmail.com>
2024-06-30 12:23:55 +0530

    automated commit #3027

--- expense-misc/main.tf
+++ expense-misc/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-709"
-    key    = "misc/jenkins-ip/terraform.tfstate"
-    region = "us-east-1"
-
-  }
-}
-
 resource "aws_route53_record" "jenkins" {
   name    = "workstation.chowdary.cloud"
   type    = "A"
--- expense-misc/state.tf
+++ expense-misc/state.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "terraform-709"
+    key    = "misc/jenkins-ip/terraform.tfstate"
+    region = "us-east-1"
+
+  }
 }
