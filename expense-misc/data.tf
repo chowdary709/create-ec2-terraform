038c7b97e2364c94df97eb28e3d163b2d2b98d21
chowdary709 <chandrahari321@gmail.com>
2024-06-30 18:41:32 +0530

    automated commit #3037

--- expense-misc/data.tf
+++ expense-misc/data.tf
@@ -1,7 +1,13 @@
-data "aws_instance" "jenkins" {
-  instance_id = "i-0f2fa8502b4f03347"
-}
-
 data "aws_instance" "workstation" {
   instance_id = "i-0769bb47be7608385"
+}
+data "aws_instances" "workstation" {
+  filter {
+    name   = "tag:Name"
+    values = ["workstation"]
+  }
+}
+
+output "workstation_instance_id" {
+  value = data.aws_instances.workstation.id
 }
