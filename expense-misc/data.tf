cfa843930723f61420e60ef39c01920dcfbfe080
chowdary709 <chandrahari321@gmail.com>
2024-06-30 18:42:39 +0530

    automated commit #3038

--- expense-misc/data.tf
+++ expense-misc/data.tf
@@ -1,13 +1,13 @@
-data "aws_instance" "workstation" {
+data "aws_instance" "Workstation" {
   instance_id = "i-0769bb47be7608385"
 }
-data "aws_instances" "workstation" {
+data "aws_instances" "Workstation" {
   filter {
     name   = "tag:Name"
-    values = ["workstation"]
+    values = ["Workstation"]
   }
 }
 
-output "workstation_instance_id" {
-  value = data.aws_instances.workstation.id
+output "Workstation_instance_id" {
+  value = data.aws_instances.Workstation.id
 }
--- expense-misc/main.tf
+++ expense-misc/main.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "workstation" {
   name    = "workstation.chowdary.cloud"
   type    = "A"
   zone_id = "Z0013695SMHQDK42GJB1"
-  records = [data.aws_instance.workstation.public_ip]
+  records = [data.aws_instance.Workstation.public_ip]
   ttl     = 10
 }
 
