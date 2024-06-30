abc9672d6e2545c495168d06b2d4c869148145b0
chowdary709 <chandrahari321@gmail.com>
2024-06-30 18:57:35 +0530

    automated commit #3044

--- expense-misc/data.tf
+++ expense-misc/data.tf
@@ -3,10 +3,10 @@ data "aws_instance" "workstation" {
 }
 
 data "aws_instance" "jenkins" {
-  instance_id = "i-0769bb47be7608385"
+  instance_id = "i-0f2fa8502b4f03347"
 }
 
 
 data "aws_instance" "sonarqube" {
-  instance_id = "i-0769bb47be7608385"
+  instance_id = "i-0e8ff690838f398fa"
 }
