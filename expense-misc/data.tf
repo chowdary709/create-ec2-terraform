57d1a495ba572970491a20c6ea052ca49e488862
chowdary709 <chandrahari321@gmail.com>
2024-06-30 18:56:30 +0530

    automated commit #3043

--- expense-misc/data.tf
+++ expense-misc/data.tf
@@ -1,3 +1,12 @@
-data "aws_instance" "Workstation" {
+data "aws_instance" "workstation" {
+  instance_id = "i-0769bb47be7608385"
+}
+
+data "aws_instance" "jenkins" {
+  instance_id = "i-0769bb47be7608385"
+}
+
+
+data "aws_instance" "sonarqube" {
   instance_id = "i-0769bb47be7608385"
 }
--- expense-misc/main.tf
+++ expense-misc/main.tf
@@ -2,11 +2,27 @@ resource "aws_route53_record" "workstation" {
   name    = "workstation.chowdary.cloud"
   type    = "A"
   zone_id = "Z0013695SMHQDK42GJB1"
-  records = [data.aws_instance.Workstation.public_ip]
+  records = [data.aws_instance.workstation.public_ip]
   ttl     = 10
 }
 
 
+resource "aws_route53_record" "jenkins" {
+  name    = "jenkins.chowdary.cloud"
+  type    = "A"
+  zone_id = "Z0013695SMHQDK42GJB1"
+  records = [data.aws_instance.jenkins.public_ip]
+  ttl     = 10
+}
+
+
+resource "aws_route53_record" "sonarqube" {
+  name    = "sonarqube.chowdary.cloud"
+  type    = "A"
+  zone_id = "Z0013695SMHQDK42GJB1"
+  records = [data.aws_instance.sonarqube.public_ip]
+  ttl     = 10
+}
 
 
 
