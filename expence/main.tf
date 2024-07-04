05260f646325f8c572b3fecce80bc8c22e9b3fb4
chowdary709 <chandrahari321@gmail.com>
2024-07-04 11:50:05 +0530

    automated commit #3170

--- expence/main.tf
+++ expence/main.tf
@@ -1,4 +1,4 @@
-module "workstation" {
+module "jenkins" {
   source                         = "./modules/ec2_instance"
   ami                            = data.aws_ami.ami.id
   subnet_id                      = var.us-east-1c
@@ -6,8 +6,8 @@ module "workstation" {
   instance_interruption_behavior = var.instance_interruption_behavior
   spot_instance_type             = var.spot_instance_type
   instance_type                  = "t3.medium"
-  instance_tag_name              = "workstation"
-  route53_record_name            = "workstation.chowdary.cloud"
+  instance_tag_name              = "jenkins"
+  route53_record_name            = "jenkins.chowdary.cloud"
 }
 
 
